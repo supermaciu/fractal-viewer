8899da2d841b1dc6b29e10df875c33fdb967c05f
Maciej Jankowski <m.jankowski2605@gmail.com>
2026-02-02 14:42:14 +0100

    something displays

--- main.c
+++ main.c
@@ -1,57 +1,81 @@
-#define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
+#define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 
-/* This function runs once at startup. */
+#define WINDOW_WIDTH 1920
+#define WINDOW_HEIGHT 1080
+
+#define SCALE 1
+
+double interpolate(int scaled, int from1, int from2, double to1, double to2) {
+    return (double) scaled/(from2-from1)*(to2-to1)+to1;
+}
+
+double magnitude(double a, double b) {
+    return SDL_sqrt(a*a + b*b);
+}
+
+bool isMandelbrot(double a, double b) {
+    double fa = 0;
+    double fb = 0;
+    int i;
+    for (i = 0; i < 1000; i++) {
+        fa = fa*fa-fb*fb + a;
+        fb = 2*fa*fb + b;
+        if (magnitude(fa, fb) > 2) break;
+    }
+
+    return (i > 800);
+}
+
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
-    /* Create the window */
-    if (!SDL_CreateWindowAndRenderer("Hello World", 800, 600, 0, &window, &renderer)) {
-        SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
-    return SDL_APP_CONTINUE;
-}
 
-/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
-{
-    if (event->type == SDL_EVENT_KEY_DOWN ||
-        event->type == SDL_EVENT_QUIT) {
-        return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
+    if (!SDL_CreateWindowAndRenderer("Fractal Viewer", WINDOW_WIDTH*SCALE, WINDOW_HEIGHT*SCALE, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
     }
-    return SDL_APP_CONTINUE;
-}
+    SDL_SetRenderLogicalPresentation(renderer, WINDOW_WIDTH*SCALE, WINDOW_HEIGHT*SCALE, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+    SDL_SetRenderScale(renderer, SCALE, SCALE);
 
-/* This function runs once per frame, and is the heart of the program. */
-SDL_AppResult SDL_AppIterate(void *appstate)
-{
-    const char *message = "Hello sigma!";
-    int w = 0, h = 0;
-    float x, y;
-    const float scale = 4.0f;
+    // draw fractal
+    uint8_t colored;
+    double a, b;
+    for (int y = 0; y < WINDOW_HEIGHT; y++) {
+        for (int x = 0; x < WINDOW_WIDTH; x++) {
+            a = interpolate(x, 0, WINDOW_WIDTH, -2, 2);
+            b = -interpolate(y, 0, WINDOW_HEIGHT, -2, 2);
+            colored = isMandelbrot(a, b) ? 0 : 255;
+            SDL_SetRenderDrawColor(renderer, colored, colored, colored, SDL_ALPHA_OPAQUE);
+            SDL_RenderPoint(renderer, x, y);
+        }
+    }
 
-    /* Center the message and scale it up */
-    SDL_GetRenderOutputSize(renderer, &w, &h);
-    SDL_SetRenderScale(renderer, scale, scale);
-    x = ((w / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE * SDL_strlen(message)) / 2;
-    y = ((h / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) / 2;
-
-    /* Draw the message */
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderDebugText(renderer, x, y, message);
     SDL_RenderPresent(renderer);
 
     return SDL_APP_CONTINUE;
 }
 
-/* This function runs once at shutdown. */
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;
+    }
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    return SDL_APP_CONTINUE;
+}
+
 void SDL_AppQuit(void *appstate, SDL_AppResult result)
 {
 }
-
